4a6a176b91943832e67b509a52c616f3af42d15b
DELISHASINGH <yashmeen1003.be23@chitkara.edu.in>
2026-04-24 15:24:50 +0530

    Final commit with all Java files

--- G31Getter.java
+++ G31Getter.java
@@ -1,9 +1,20 @@
 public class G31Getter {
-    int student s = 
+    a s = new a();
+    public static void main(String[] args) {
+        G31Getter g = new G31Getter();
+        System.out.println(g.s.getAge());
+        System.out.println(g.s.getName());
+    }
 }
 class a{
     private int age;
     private String name;
-
+    public int getAge(){
+        return age;
+    }
+    public String getName(){
+        return name;
+    }
+    
 
 }
